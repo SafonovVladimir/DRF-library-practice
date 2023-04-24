dde8867dcc38c2c9535c68258ddf5be257d3749f
Safonov Volodymyr <34244814+SafonovVladimir@users.noreply.github.com>
2023-04-24 14:30:05 +0300

    added Borrowing

--- borrowing/models.py
+++ borrowing/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Borrowing(models.Model):
+    borrow_date = models.DateField(auto_now_add=True)
+    expected_date = models.DateField()
+    actual_date = models.DateField()
+    book_id = models.ForeignKey("Book", on_delete=models.CASCADE)
+    user_id = models.ForeignKey("User", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return (f"{self.book_id.title}"
+                f"({self.borrow_date}-{self.expected_date})")
