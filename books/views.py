75ce15ba6350b3d05dd4efd0eceb049d97d944ff
viktoria-rybenchuk <rybenchukv@gmail.com>
2023-04-24 14:05:15 +0200

    Added custom permission class IsAdminOrReadOnly

--- books/views.py
+++ books/views.py
@@ -1,14 +1,22 @@
+from typing import Type
+
 from rest_framework import viewsets
+from rest_framework.serializers import Serializer
 
 from books.models import Book
-from books.serializers import BookSerializer, BookListSerializer
+from books.permissions import IsAdminOrReadOnly
+from books.serializers import (
+    BookSerializer,
+    BookListSerializer
+)
 
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = (IsAdminOrReadOnly, )
 
-    def get_serializer_class(self):
+    def get_serializer_class(self) -> Type[Serializer]:
         if self.action == "list":
             return BookListSerializer
         return self.serializer_class
