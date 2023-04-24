9ce0e20f46268e12cd1efe4d202cc1aedbced9a4
viktoria-rybenchuk <rybenchukv@gmail.com>
2023-04-24 13:49:49 +0200

    Implemented CRUD operations for managing book data.

---
+++ books/serializers.py
@@ -0,0 +1,23 @@
+from rest_framework import serializers
+
+from books.models import Book
+
+
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = (
+            "id",
+            "title",
+            "author",
+            "cover",
+            "image",
+            "inventory",
+            "daylee_fee"
+        )
+
+
+class BookListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = ("id", "title", "author", "inventory")
