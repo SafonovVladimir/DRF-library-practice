2143ab6b910584de6085abe5aed9512a6d80def0
viktoria-rybenchuk <rybenchukv@gmail.com>
2023-04-24 16:06:47 +0200

    Added Author model and added ManytoMany relation to Books model

--- books/models.py
+++ books/models.py
@@ -1,17 +1,23 @@
 from django.db import models
 
 
+class Author(models.Model):
+    first_name = models.CharField(max_length=63)
+    last_name = models.CharField(max_length=63)
+    pseudonim = models.CharField(max_length=63, blank=True, null=True)
+
+
 class Book(models.Model):
     class CoverChoices(models.TextChoices):
         HARD = "HARD"
         SOFT = "SOFT"
 
     title = models.CharField(max_length=63)
-    author = models.CharField(max_length=255)
+    author = models.ManyToManyField(Author, related_name="books")
     cover = models.CharField(max_length=63, choices=CoverChoices.choices)
     image = models.ImageField(upload_to="book_cover", blank=True, null=True)
     inventory = models.IntegerField()
-    daylee_fee = models.IntegerField()
+    daily_fee = models.IntegerField()
 
     def __str__(self) -> str:
         return self.title
