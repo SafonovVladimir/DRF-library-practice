2546c7dad15143fc193935382cae91dd829aa199
Anatolii Pozniak <a13101990a@gmail.com>
2023-04-25 17:12:28 +0300

    add return_borrowing endpoint to urls

--- borrowing/urls.py
+++ borrowing/urls.py
@@ -8,6 +8,11 @@ router.register("borrowing", BorrowingViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
+    path(
+        "borrowing/<int:pk>/return_borrowing/",
+        BorrowingViewSet.as_view({"post": "return_borrowing"}),
+        name="return_borrowing"
+    ),
 ]
 
 app_name = "borrowing"
