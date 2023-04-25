602965669a522a8955f1e7f60c7821cb1f702d07
Rodion Prodaivoda <hapyto371@gmail.com>
2023-04-25 10:57:15 +0300

    views filter for book

--- books/views.py
+++ books/views.py
@@ -20,3 +20,29 @@ class BookViewSet(viewsets.ModelViewSet):
         if self.action == "list":
             return BookListSerializer
         return self.serializer_class
+
+    @staticmethod
+    def _params_to_ints(qs):
+        """Convert a list of string IDs to a list of integers"""
+        return [int(str_id) for str_id in qs.split(",")]
+
+    def get_queryset(self):
+        """Retrieve the books with filters"""
+        title = self.request.query_params.get("title")
+        authors = self.request.query_params.get("authors")
+        cover = self.request.query_params.get("cover")
+
+        queryset = self.queryset
+
+        if title:
+            queryset = queryset.filter(title__icontains=title)
+
+        if authors:
+            authors_ids = self._params_to_ints(authors)
+            queryset = queryset.filter(authors__id__in=authors_ids)
+
+        if cover:
+            cover_ids = self._params_to_ints(cover)
+            queryset = queryset.filter(cover__id__in=cover_ids)
+
+        return queryset.distinct()
