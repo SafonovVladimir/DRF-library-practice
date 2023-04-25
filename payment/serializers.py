6938c16a08445437b0622b280ef57d9073683154
Rodion Prodaivoda <hapyto371@gmail.com>
2023-04-25 17:31:47 +0300

    payment app

---
+++ payment/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+
+# from borrowing.serializers import BorrowingSerializer
+from payment.models import Payment
+
+
+class PaymentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Payment
+        read_only_fields = ("id", "session_url", "session_id",)
+        fields = ("id", "status", "type", "session_url", "session_id", "borrowing", "amount",)
+
+
+class PaymentListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Payment
+        read_only_fields = ("id", "session_url", "session_id",)
+        fields = ("id", "status", "type", "session_url", "session_id", "borrowing", "amount",)
+
