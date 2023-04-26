179b8c775e3bd214c067395fdb5cb821b0884e63
Safonov Volodymyr <34244814+SafonovVladimir@users.noreply.github.com>
2023-04-26 13:43:04 +0300

    Change notifications

--- library_config/settings.py
+++ library_config/settings.py
@@ -165,8 +165,8 @@ TELEGRAM_CHAT_ID = 724066917
 Q_CLUSTER = {
     "name": "DjangORM",
     "workers": 4,
-    "timeout": 90,
-    "retry": 120,
+    "timeout": 30,
+    "retry": 60,
     "queue_limit": 50,
     "bulk": 10,
     "orm": "default"
